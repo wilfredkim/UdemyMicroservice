2742007eebf8b33f0a74a46af601e3d592ade4cf
Wilfred <wilfredkim5@gmail.com>
2023-06-05 23:24:40 +0300

    adding currency exchange controller

---
+++ CurrencyExchangeService/src/main/java/com/wilfred/currencyexchange/controller/CurrencyExchangeController.java
@@ -0,0 +1,9 @@
+package com.wilfred.currencyexchange.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/currencyconverter")
+public class CurrencyExchangeController {
+}
