4f77e8893496daa1acc0061f0e35584efae920d0
Wilfred <wilfredkim5@gmail.com>
2023-06-28 13:50:50 +0300

    updates in microservices  currency exchange and convertion

--- CurrencyConvertionService/src/main/java/com/wilfred/currencyconversion/controller/CurrencyConversionController.java
+++ CurrencyConvertionService/src/main/java/com/wilfred/currencyconversion/controller/CurrencyConversionController.java
@@ -3,24 +3,54 @@ package com.wilfred.currencyconversion.controller;
 import com.wilfred.currencyconversion.model.CurrencyConversion;
 import com.wilfred.currencyconversion.service.CurrencyConversionProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/v1/api/currencyconversion")
 public class CurrencyConversionController {
     @Autowired
     CurrencyConversionProxy currencyConversionProxy;
-
+    @Autowired
+    private RestTemplate restTemplate;
     @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from,
                                                                @PathVariable String to,
                                                                @PathVariable BigDecimal quantity) {
         CurrencyConversion currencyConversion = currencyConversionProxy.convert(from, to, quantity);
         return new CurrencyConversion(currencyConversion.getId(), from, to, currencyConversion.getSpotRate(), quantity, currencyConversion.getTotalCalculatedAmount(), currencyConversion.getEnvironment());
+    }
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ) {
+
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from",from);
+        uriVariables.put("to",to);
+        uriVariables.put("spotRate",BigDecimal.ONE.toString());
+
+        ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity
+                ("http://localhost:1072/api/v1/currencyconverter/convert/{from}/{to}/{spotRate}",
+                        CurrencyConversion.class, uriVariables);
+
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,
+                currencyConversion.getSpotRate(),
+                quantity.multiply(currencyConversion.getSpotRate()),
+                currencyConversion.getEnvironment()+ " " + "rest template");
+
     }
 }
--- CurrencyExchangeService/src/main/java/com/wilfred/currencyexchange/controller/CurrencyExchangeController.java
+++ CurrencyExchangeService/src/main/java/com/wilfred/currencyexchange/controller/CurrencyExchangeController.java
@@ -3,15 +3,18 @@ package com.wilfred.currencyexchange.controller;
 import com.wilfred.currencyexchange.model.CurrencyExchange;
 import com.wilfred.currencyexchange.repository.CurrencyExchangeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/currencyconverter")
 @RequiredArgsConstructor
+@Slf4j
 public class CurrencyExchangeController {
 
 
@@ -21,7 +24,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/convert/{from}/{to}/{spotRate}")
     public CurrencyExchange convert(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal spotRate) {
-        //CurrencyExchange currencyExchange = new CurrencyExchange(1L, from, to, spotRate, "");
+        log.info("{from}::" + from + "  {to}:::::: " + to + "  {spotRate}:::::" + spotRate);
         CurrencyExchange currencyExchange = repository.findCurrencyExchangeByFromAndTo(from, to);
         if (currencyExchange == null)
             throw new RuntimeException("Cannot find currency with from currency " + from + " to " + to);
@@ -31,7 +34,13 @@ public class CurrencyExchangeController {
     }
 
     @PostMapping
-    public CurrencyExchange saveOrUpdate(@RequestBody  CurrencyExchange currencyExchange) {
+    public CurrencyExchange saveOrUpdate(@RequestBody CurrencyExchange currencyExchange) {
         return repository.save(currencyExchange);
+    }
+
+    @GetMapping
+    public List<CurrencyExchange> getList() {
+        return repository.findAll();
+
     }
 }
